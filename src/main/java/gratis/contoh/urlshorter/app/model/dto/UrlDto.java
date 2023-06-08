0ce89b921deb273e5b253080f8fdf6b2ebd56094
Andri Nur Rochman <ceoandri@gmail.com>
2023-06-08 09:01:47 +0700

    add validation minimum length

--- src/main/java/gratis/contoh/urlshorter/app/model/dto/UrlDto.java
+++ src/main/java/gratis/contoh/urlshorter/app/model/dto/UrlDto.java
@@ -3,7 +3,7 @@ package gratis.contoh.urlshorter.app.model.dto;
 import java.io.Serializable;
 
 import jakarta.validation.constraints.NotNull;
-
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +18,7 @@ public class UrlDto implements Serializable {
 	private static final long serialVersionUID = 2128066004562579809L;
 
 	@NotNull
+	@Size(min = 7, message = "The url must have a minimum length of 7 characters")
 	private String url;
 
 }
